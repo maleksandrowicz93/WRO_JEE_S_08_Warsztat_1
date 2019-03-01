37c8b28e236920b3db840649a295fa5773534ad4
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-03-01 16:22:36 +0100

    Dopracowanie mechanizmu zgadywania

--- src/maleksandrowicz93/workshops/guess_number/GuessNumberApp.java
+++ src/maleksandrowicz93/workshops/guess_number/GuessNumberApp.java
@@ -8,26 +8,35 @@ public class GuessNumberApp {
     public static void main(String[] args) {
 
         Random r = new Random();
-        int numberToGuess = r.nextInt(100)+1;
+        int numberToGuess = r.nextInt(100) + 1;
         lookigForNumberByUser(numberToGuess);
 
     }
 
-    static void lookigForNumberByUser (int numberToGuess) {
+    static void lookigForNumberByUser(int numberToGuess) {
 
         Scanner scanner = new Scanner(System.in);
         int checkedNumber = 0;
         int howManyTimes = 0;
         System.out.print("Let's play the game. Try to find a random number. Write value You want to check: ");
-        while (checkedNumber != numberToGuess) {
+//        while (!scanner.hasNextInt()) {
+//            System.out.print("It is not a number! Write a number, please: ");
+//            continue;
+//        }
+        do {
             checkedNumber = scanner.nextInt();
-            if (checkedNumber > numberToGuess) {
-                System.out.print("Za dużo! Spróbuj jeszcze raz: ");
-            } else if (checkedNumber < numberToGuess) {
-                System.out.print("Za mało! Spróbuj jeszcze raz: ");
-            }
+            String isNUmber = checkedNumber + "";
             howManyTimes++;
-        }
+            if (checkedNumber > numberToGuess) {
+                System.out.print("Too many! Let's try again: ");
+            } else if (checkedNumber < numberToGuess) {
+                System.out.print("Too little! Let's try again: ");
+            } else {
+                break;
+            }
+
+        } while (true);
+
 
         System.out.println("You managed to guess a random number. You needed " + howManyTimes + " attempts.");
 
