b338dbb6045f34d0edc2b0a0644c244f46ddcc1d
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2019-03-03 23:35:18 +0100

    Komunikacja z uzytkownikiem i eliminacja niepoprawnych wejść

--- src/maleksandrowicz93/workshops/guess_number/dice_simulator/DiceSimulatorApp.java
+++ src/maleksandrowicz93/workshops/guess_number/dice_simulator/DiceSimulatorApp.java
@@ -1,5 +1,6 @@
 package maleksandrowicz93.workshops.guess_number.dice_simulator;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class DiceSimulatorApp {
@@ -7,8 +8,23 @@ public class DiceSimulatorApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj ilość rzutów kością: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.err.print("Niewłasciwa wartość! Podaj ilość kości: ");
+        }
         int numberOfDices = scanner.nextInt();
+        System.out.print("Podaj ilość ścianek kości: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.err.print("Niewłasciwa wartość! Podaj rodzaj kości: ");
+        }
         int typeOfDices = scanner.nextInt();
+        System.out.print("Podaj modyfikator rzutu: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.err.print("Niewłasciwa wartość! Podaj modyfikator rzutu: ");
+        }
         int modifier = scanner.nextInt();
         String diceCode = numberOfDices + "D" + typeOfDices + "+" + modifier;
         diceRolling(diceCode);
@@ -18,6 +34,7 @@ public class DiceSimulatorApp {
 
     static void diceRolling (String diceCode) {
 
+//        Random random = new Random();
         System.out.println(diceCode);
 
     }
